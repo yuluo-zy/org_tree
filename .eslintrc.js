1ac3a0df849a8b9b151d45ae6f59621e6b049167
yuluo <yuluozy@outlook.com>
2022-01-09 01:23:52 +0800

    add 格式化文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     "@vue/typescript/recommended",
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint",
+    'prettier'
   ],
   parserOptions: {
     ecmaVersion: 2020,
@@ -16,6 +17,37 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-var': 'error',
+    'semi': 'off',
+    // 优先使用 interface 而不是 type
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'vue/html-indent': [
+      'error',
+      4,
+      {
+        attribute: 1,
+        baseIndent: 1,
+        closeBracket: 0,
+        alignAttributesVertically: true,
+        ignores: [],
+      },
+    ],
+    // 关闭此规则 使用 prettier 的格式化规则， 感觉prettier 更加合理，
+    // 而且一起使用会有冲突
+    'vue/max-attributes-per-line': ['off'],
+    // 强制使用驼峰命名
+    'vue/component-name-in-template-casing': [
+      'error',
+      'PascalCase',
+      {
+        registeredComponentsOnly: false,
+        ignores: [],
+      },
+    ],
   },
   overrides: [
     {
