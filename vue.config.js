828a8c5a0227e0c2e238d8cfaa82faae690b8978
yuluo <yuluozy@outlook.com>
2022-01-09 22:09:33 +0800

    add 示例文件

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    pages: {
+        index: {
+            // 修改入口
+            entry: 'examples/main.ts',
+            template: 'public/index.html',
+            filename: 'index.html'
+        }
+    },
+    chainWebpack: config => {
+        config.module
+            .rule('js')
+            .include.add('/packages')
+            .end()
+            .use('babel')
+            .loader('babel-loader')
+            .tap(options => {
+                return options;
+            });
+    }
+};
